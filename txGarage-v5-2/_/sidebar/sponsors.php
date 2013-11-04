b2e89e75078b6e4a0e2b37972f2eedfab58a0ab2
Adam Moore <adamaoc@gmail.com>
2013-11-04 12:34:58 -0500

    updated sponsors for new campain

--- txGarage-v5-2/_/sidebar/sponsors.php
+++ txGarage-v5-2/_/sidebar/sponsors.php
@@ -7,6 +7,7 @@
 	<img src="http://txgarage.com/images/2013/11/2014_Silverado1500_strong_Static_US_ENG_v1_260x260.jpg" target="_blank" alt="2014 Chevrolet Silverado Strong as a Silverado">
 	<img src="http://ad.doubleclick.net/ad/N8334.127315.CENTRO/B7923611.16;sz=1x1;ord=[timestamp]?">
 </a>
+
 <a href="http://www.mediatemple.net#a_aid=512fd1cf8a22a&amp;a_bid=f814c65b" target="_top" rel="nofollow" style="float: left; margin: 7px;">
 	<img src="https://affiliate.mediatemple.net/accounts/default1/banners/125x125.jpg" alt="" width="125" height="125" />
 </a>
