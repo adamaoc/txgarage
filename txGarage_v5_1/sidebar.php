4a30783270ba1b5850f92b3d26c20174c2fbf604
Adam Moore <adamaoc@gmail.com>
2013-11-25 14:27:52 -0600

    Added google plus counter - quick and dirty -

--- txGarage_v5_1/sidebar.php
+++ txGarage_v5_1/sidebar.php
@@ -15,9 +15,24 @@
     <div class="sidebar-sponsors">
     	<h2>Sponsors</h2>
     	<?php include("_/sidebar/sponsors.php"); ?>
-		</div>
+    </div>
 		<div class="clear"></div>
-
+		
+    <div class="sidebar-gplus">
+    	<!-- Place this tag where you want the +1 button to render. -->
+	<div class="g-plusone"></div>
+	
+	<!-- Place this tag after the last +1 button tag. -->
+	<script type="text/javascript">
+	  (function() {
+	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	    po.src = 'https://apis.google.com/js/platform.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	  })();
+	</script>
+    </div>
+		<div class="clear"></div>
+    
     <div class="sidebar-tag-search">
     	<h2>Search by tag</h2>
     		<?php wp_tag_cloud('smallest=8&largest=18&number=25'); ?>
