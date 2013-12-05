18f2a835e56e5467498d1835a3d38fd731b7cb4e
Adam Moore <adamaoc@gmail.com>
2013-12-05 10:01:25 -0600

    cleaning up homepage markup

--- txGarage_v5_1/home_page.php
+++ txGarage_v5_1/home_page.php
@@ -4,19 +4,14 @@ Template Name: home_template
 ?>
 	
 <?php get_header(); ?>
+
 <div id="inner-wrap">
-<?php include("_/home/home_top.php"); ?>
+	<?php include("_/home/home_top.php"); ?>
 
-<?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
 
-<?php include("_/home/main_list.php"); ?>
+	<?php include("_/home/main_list.php"); ?>
 	<div class="clear"></div>
 </div>
-
-<div id="outter_news"><div class="entry"><h2>This is this</h2><p>That is that... </p></div></div>
-
-		<h2 id="description_index">
-			- auto vehicle - automobile - texas garage - texas car enthusiast - benz of mercedes benz - mercedes benz mercedes benz - auto dealers in - auto dealers - for auto dealers - auto new - new auto - auto motor vehicle - automotive auto repair - auto insurance texas - newcar - texas dallas - houston texas - austin texas - san antonio texas - news - texas car news - texas motorsports - motorsports - automotive reviews - car reviews - first drive reviews - new reviews - used reviews - dealer reviews - ford reviews - chevrolet reviews - chevy reviews - hyundai reviews - audi reviews - volkswagen reviews - vw reviews - honda reviews - acura reviews - chrysler reviews - ram reviews - dodge reviews - kia reviews - bmw reviews - mercedez benz reviews - mb reviews - vw reviews - automotive news - car news - first drive news - new news - used news - dealer news - ford news - chevrolet news - chevy news - hyundai news - audi news - volkswagen news - vw news - honda news - acura news - chrysler news - ram news - dodge news - kia news - bmw news - mercedez benz news - mb news - vw news - auto in texas - mercedes b - auto review - review auto - racing driving - driving and racing - auto motor sports - motor sport 4 - new motor vehicle - 2012 auto show - auto association - first automotive - suzuki review - suzuki news - magazine auto - new 2012 honda - auto motor vehicle - auto mart - suzuki kisashi - 2011 vehicle - driving and racing - iphone - mobel - moble - mobile - website - web site - blog - auto blog - automotive blog - autoblog - fast and loud - texas tv show - texas fast and loud - evoque - range evoque - evoque range - concept automobile - automobile industry in the us - automobile industry future - auto nissan
-		</h2>
 
 <?php get_footer(); ?>
