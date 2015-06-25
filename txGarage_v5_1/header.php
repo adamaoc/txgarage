e8d0a7602df5b252abbd62acd42656fd5c575e0d
adam moore <adamaoc@gmail.com>
2015-06-25 06:40:40 -0500

    added google tag manager code to v5.1 design

--- txGarage_v5_1/header.php
+++ txGarage_v5_1/header.php
@@ -145,6 +145,16 @@
 				}
 			
 			?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N3556S"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N3556S');</script>
+<!-- End Google Tag Manager -->
+
 	<!-- txGarage is part of the ampnet(media) group -->
 	<div id="page-wrap">
 
