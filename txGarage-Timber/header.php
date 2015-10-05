68acb08abbcf56c1ea629ab6d51c7726a8fba4ce
adam moore <adamaoc@gmail.com>
2015-10-05 06:33:16 -0500

    added favicons

--- txGarage-Timber/header.php
+++ txGarage-Timber/header.php
@@ -11,6 +11,24 @@
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
 
         <!-- linkup Favicon and apple icons -->
+		<link rel="apple-touch-icon" sizes="57x57" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?= get_template_directory_uri(); ?>/assets/favicons/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/assets/favicons/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/assets/favicons/favicon-194x194.png" sizes="194x194">
+		<link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/assets/favicons/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/assets/favicons/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/assets/favicons/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="<?= get_template_directory_uri(); ?>/assets/favicons/manifest.json">
+		<meta name="msapplication-TileColor" content="#2d89ef">
+		<meta name="msapplication-TileImage" content="<?= get_template_directory_uri(); ?>/assets/favicons/mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
